32ff67bb01f8fd16c3d70b259c253950f9b4c279
Michelle Wrage <michelle.wrage@student.uni-luebeck.de>
2025-04-21 10:37:04 +0200

    readjusted mobile view

--- client/src/components/BookingScreen.jsx
+++ client/src/components/BookingScreen.jsx
@@ -10,7 +10,7 @@ function BookingScreen() {
   const { tripRequested, waitingForResponse } = useContext(AppContext);
 
   return (
-      <div className="flex flex-col w-full max-md:pb-8 h-full text-zinc-800 bg-gray-100 rounded-3xl inter-300">
+      <div className="flex flex-col w-full h-full text-zinc-800 bg-gray-100 rounded-3xl inter-300">
         <Map />
         {tripRequested ? <RequestResults /> : waitingForResponse ? <LoadingScreen /> : <BookingArea />}
       </div>
--- client/src/components/MobileDevice.jsx
+++ client/src/components/MobileDevice.jsx
@@ -2,12 +2,12 @@
 function MobileDevice({ content }) {
   return (
     <>
-        <section className='h-screen w-full md:p-2 flex items-center justify-center'>
-        <div className='flex items-center justify-center h-full w-full md:w-sm md:bg-radial md:from-gray-500 md:to-gray-900 md:border-4 md:border-black md:rounded-4xl md:p-4'>
-            <div className='md:z-100 absolute self-start flex items-center justify-center h-6 w-36 bg-linear-to-b from-gray-800 to-gray-900 rounded-b-2xl'>
+        <section className='h-screen w-full p-2 flex items-center justify-center'>
+        <div className='flex items-center justify-center h-screen w-sm bg-radial from-gray-500 to-gray-900 border-4 border-black rounded-4xl p-4'>
+            <div className='z-100 absolute self-start flex items-center justify-center h-6 w-36 bg-linear-to-b from-gray-800 to-gray-900 rounded-b-2xl'>
                 <div className='w-1/2 rounded-full bg-gray-700 h-1 mb-2'></div>
             </div>
-            <div className='h-full w-full bg-gray-100 md:rounded-3xl'>
+            <div className='h-full w-full bg-gray-100 rounded-3xl'>
                 <div className="h-full w-full">
                     {content}
                 </div>
