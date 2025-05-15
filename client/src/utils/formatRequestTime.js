24a35e48ee77813d73658ca774a72e8fc635a3b7
Michelle Wrage <michelle.wrage@student.uni-luebeck.de>
2025-05-16 01:30:13 +0200

    fixed time conversion issue

--- client/src/utils/formatRequestTime.js
+++ client/src/utils/formatRequestTime.js
@@ -1,7 +1,9 @@
+import { parse } from 'date-fns'
+import { de } from 'date-fns/locale'
 
 export const formatRequestedTime = ([dateStr, timeStr]) => {
     const cleanedDate = dateStr.replace(/^.*?, /, '') + ' 2025 ' + timeStr;
-    const date = new Date(cleanedDate);
+    const date = parse(cleanedDate, 'd. MMMM yyyy HH:mm', new Date(), { locale: de });
     const formatted = date.toLocaleString('sv-SE').replace(' ', 'T');
     return formatted;
 }
